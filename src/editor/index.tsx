3f00601818f57ec82c52f28f31a408bc4dc52645
ireh(서인아) <ireh.s@kakaoenterprise.com>
2023-06-15 18:29:59 +0900

    초기 설정

---
+++ src/editor/index.tsx
@@ -0,0 +1,12 @@
+import ReactMarkdown from "react-markdown";
+
+const ReactMDEditor = () => {
+
+    return (
+        <ReactMarkdown
+            children = {''}
+        />
+    )
+}
+
+export default ReactMDEditor;
